dd2660f893d88897059d3dc3824f1328409b4e08
admin <bugsteam9912@gmail.com>
2025-10-28 19:38:43 +0900

    rsprsp

--- src/MiniGameMain.java
+++ src/MiniGameMain.java
@@ -11,30 +11,26 @@ public class MiniGameMain {
         while (true) {
             System.out.println("[1] 업앤다운 [2] 가위바위보 [3] 숫자야구 [4] 종료");
             System.out.print(">> ");
-            int select = sc.nextInt();
-            switch (select) {
+            switch (sc.nextInt()) {
                 case 1:
                     upAndDown();
                     continue;
                 case 2:
                     rsp();
-                    break;
+                    continue;
                 case 3:
 
                     break;
                 case 4:
                     System.exit(1);
-                    break;
                 default:
                     System.out.println("잘못 입력하셨습니다.");
             }
         }
     }
 
-    private static void upAndDown() {
+    public static void upAndDown() {
         // 업앤 다운
-        // 컴터가 1~100 사이의 숫자를 랜덤 생성
-        // 유저의 입력을 받고 1~100 사이가 아니라면 재입력 받기
         int comNum = (int)(Math.random() * 100 + 1);
         int count = 1;
         System.out.println("7번의 기회가 주어집니다.");
@@ -62,13 +58,63 @@ public class MiniGameMain {
             } else {
                 System.out.println("정답!");
                 break;
+            }
+        }
+    }
+
+
+    public static void rsp() {
+        // 가위바위보
+        System.out.println("----가위바위보----");
+        while (true) {
+            int user = 0;
+            Integer comRsp = (int)(Math.random() * 3 + 1);
+            System.out.print("가위바위보 선택 : ");
+            String userRsp = sc.next();
+
+            if (!(userRsp.equals("가위") || userRsp.equals("바위") || userRsp.equals("보"))) {
+                System.out.println("가위, 바위, 보 중에서만 입력하세요");
+                continue;
+            }
+
+            if (userRsp.equals("가위")) user = 1;
+            if (userRsp.equals("바위")) user = 2;
+            if (userRsp.equals("보")) user = 3;
+
+                // 무승부
+            if (user == comRsp) {
+                System.out.println("무승부");
+                continue;
+                // 1 가위
+            }
+            if(comRsp == 1) {
+                System.out.println("컴퓨터 : 가위");
+                  if (user == 2) {
+                    System.out.println("결과 : 이김");
+                    break;
+                } else {
+                    System.out.println("결과 : 패배");
+                }
+                  // 2번 바위
+            } else if (comRsp == 2) {
+                System.out.println("컴퓨터 : 바위");
+                if (user == 1) {
+                    System.out.println("결과 : 패배");
+                } else {
+                    System.out.println("결과 : 이김");
+                    break;
+                }
+                  // 3번 보
+            } else {
+                System.out.println("컴퓨터 : 보");
+                if (user == 1) {
+                    System.out.println("결과 : 이김");
+                    break;
+                } else {
+                    System.out.println("결과 : 패배");
                 }
             }
         }
-
-
-    private static void rsp() {
-
     }
 }
 
